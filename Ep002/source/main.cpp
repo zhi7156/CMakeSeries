7c98e97e27932b14f8b97be4f4f5d11d0b400ab8
zhi <zhi@zhi-ubuntu22.04-virtual-machine>
2023-08-01 21:20:23 +0800

    Apply changes to main.cpp from linux

--- Ep002/source/main.cpp
+++ Ep002/source/main.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
+#include <concepts>
+
+template <typename T>
+requires std::integral<T>
+T add(T a, T b){
+    return a+b;
+}
+
 
 int main(){
 
     std::cout << "Hello World" << std::endl;
     std::cout << "************************************************************" << std::endl;
-
+    std::cout << "The sum is " << add(3, 7) << std::endl;
 
     
     std::cout << "Program well done" << std::endl;
