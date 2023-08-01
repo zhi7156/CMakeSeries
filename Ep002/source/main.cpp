3858eb524077d237e912e9fea46f96fa8a5c1cd8
zhi7156 <1973608824@qq.com>
2023-08-01 08:04:40 +0800

    Episode2 code

---
+++ Ep002/source/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(){
+
+    std::cout << "Hello World" << std::endl;
+    std::cout << "************************************************************" << std::endl;
+
+
+    
+    std::cout << "Program well done" << std::endl;
+    return 0;
+}
